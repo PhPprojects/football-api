a873a1e6c572e88363ee1a14eef02da084af8a6d
spolischook <spolischook@gmail.com>
2013-01-24 13:14:22 +0200

    Create new Entities

---
+++ src/Knp/ChallengeBundle/Entity/Team.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Knp\ChallengeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Team
+ *
+ * @ORM\Table(name="team")
+ * @ORM\Entity(repositoryClass="Knp\ChallengeBundle\Entity\TeamRepository")
+ */
+class Team
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var
+     *
+     * @ORM\OneToMany(targetEntity="Team", mappedBy="homeTeam")
+     */
+    private $homeTeamGames;
+
+    /**
+     * @var
+     *
+     * @ORM\OneToMany(targetEntity="Team", mappedBy="awayTeam")
+     */
+    private $awayTeamGames;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Team
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
