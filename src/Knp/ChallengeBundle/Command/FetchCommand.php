7b3fa4f35944d0733584836ae5d923e014e37417
spolischook <spolischook@gmail.com>
2013-01-25 07:00:26 +0200

    Created Console Command

---
+++ src/Knp/ChallengeBundle/Command/FetchCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Knp\ChallengeBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FetchCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('fetch:data')
+            ->setDescription('That fetches data from Soccerway Premier League')
+            ->addOption('debug', null, InputOption::VALUE_NONE, 'If set, will be view debug info')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('debug')) {
+
+        }
+        $helper = $this->getContainer()->get('challenge.helper');
+
+        if ($helper->fetchData()) {
+            $output->writeln('Fetch data success!');
+        }
+        else {
+            $output->writeln('Something went wrong :(');
+        }
+
+    }
+}
